e53ca6a72dca8c15ab5a8f90d7d0f6f9f068d303
Dalton Cherry <daltoniam@gmail.com>
2015-02-20 15:14:49 -0800

    db resizing logic

--- dash.go
+++ dash.go
@@ -25,7 +25,7 @@ type Dash struct {
 	fileQueue chan pair         //channel used for queuing database file writes
 	dbFile    string            //the location of the database file. Default is ./dash.db
 	DoSync    bool              //should it be fast or safe?
-	dbSize    int               //the size of the db file in MB
+	maxDBSize float64           //the max size of the db file in MB. Default is 100MB. Be careful of making this size to small as it is possible to get caught in a infinite loop of shrinking the db.
 }
 
 //used to communicate actions across the channels
@@ -37,7 +37,7 @@ type pair struct {
 
 //create a new Dash instance
 func New() (*Dash, error) {
-	d := Dash{store: make(map[string]string), workQueue: make(chan pair), fileQueue: make(chan pair), dbFile: "dash.db", dbSize: 100}
+	d := Dash{store: make(map[string]string), workQueue: make(chan pair), fileQueue: make(chan pair), dbFile: "dash.db", maxDBSize: 100}
 	d.DoSync = true
 	d.loadData()
 	go d.start()
@@ -105,6 +105,8 @@ func (d *Dash) startDiskQueue() error {
 //channel that waits and preforms file opts that are written to it.
 func (d *Dash) runDiskQueue(f *os.File) {
 	defer f.Close()
+	info, _ := f.Stat()
+	size := info.Size()
 	for {
 		select {
 		case pair := <-d.fileQueue:
@@ -115,14 +117,45 @@ func (d *Dash) runDiskQueue(f *os.File) {
 			if pair.code == RemoveCode {
 				action = RemoveAction
 			}
-			f.Write([]byte(fmt.Sprintf("%d\n%s%d\n%s%d\n%s", len(action), action, len(pair.key), pair.key, len(pair.value), pair.value)))
+			b := buildDiskAction(action, pair.key, pair.value)
+			f.Write(b)
+			size += int64(len(b))
 			//we can and should optimize this. e.g: Time interval (1 second) to do a sync, how many opts are pending (only sync every 100 opts when load is high),
 			//buffer the written content, etc. allow for varying levels of safety vs speed.
 			if d.DoSync {
 				f.Sync()
 			}
+			//check if the db size hash been exceeded
+			mbSize := float64(size) * 0.000001
+			if mbSize > d.maxDBSize {
+				//transfer the content to the temp.db
+				tempPath := "temp.db"
+				tempFile, err := os.Create(tempPath)
+				if err != nil {
+					//not sure what to do here but log
+					fmt.Println("unable to create temp db file. That is a very serious error:", err)
+				} else {
+					for key, val := range d.store {
+						b := buildDiskAction(WriteAction, key, val)
+						tempFile.Write(b)
+					}
+					tempFile.Sync()
+					os.Remove(d.dbFile)
+					os.Rename(tempPath, d.dbFile)
+					tempFile.Close()
+					f.Close()
+					f, _ = os.OpenFile(d.dbFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+					info, _ := f.Stat()
+					size = info.Size()
+				}
+			}
 		}
 	}
+}
+
+//builds a disk action for writing
+func buildDiskAction(action, key, value string) []byte {
+	return []byte(fmt.Sprintf("%d\n%s%d\n%s%d\n%s", len(action), action, len(key), key, len(value), value))
 }
 
 //loads the data stored in the db file into the store.
